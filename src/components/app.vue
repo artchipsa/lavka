8dcbc129ded860d891d9c64ea98013fe30f473de
artchipsa <art@chipsa.ru>
2017-09-07 15:02:26 +0700

    v1.1.0 final

--- src/components/app.vue
+++ src/components/app.vue
@@ -169,7 +169,7 @@
         mounted(){
             
             setTimeout(() => {
-                alert($('.logo').css('opacity'));
+                alert($('.logo svg').css('opacity'));
             }, 2000)
 
             this.uiColorChange();
